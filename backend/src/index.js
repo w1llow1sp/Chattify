402636887fa5d03249df97e86631bf397a486862
Mary <mari.front.74@gmail.com>
2025-05-23 21:10:43 +0500

    # 1. Login and Signup pages  * add  routes  * add axios  * add zustand store for login, logout and signup  * protect routes  (semi-ready)  * add templates for pages  * fix backend cors policy  * add decoration component for login and signup page

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,6 +4,7 @@ import authRoutes from "./routes/auth.route.js";
 import messageRoutes from './routes/message.route.js'
 import {connectDB} from "./lib/db.js";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 dotenv.config()
 
@@ -12,9 +13,16 @@ const PORT = process.env.PORT;
 
 app.use(express.json())
 app.use(cookieParser())
+app.use(
+    cors({
+        origin: "http://localhost:5173",
+        credentials: true,
+    })
+);
 
 app.use('/api/auth', authRoutes)
 app.use('/api/message', messageRoutes);
+
 
 
 app.listen(PORT, () => {
