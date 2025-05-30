f84e7ca73f5c3431b01b2651f20060dff227db8f
Mary <mari.front.74@gmail.com>
2025-05-30 22:47:01 +0500

    chore: add development chore

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,10 +6,12 @@ import {connectDB} from "./lib/db.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import { app, server } from "./lib/socket.js";
+import path from "path";
 
 dotenv.config()
 
 const PORT = process.env.PORT;
+const __dirname = path.resolve();
 
 //app.use(express.json())
 app.use(express.json({limit: '50mb'}));
@@ -26,7 +28,12 @@ app.use(
 app.use('/api/auth', authRoutes)
 app.use('/api/messages', messageRoutes);
 
-
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../frontend/dist')))
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, '../frontend', 'dist', 'index.html'))
+    }
+}
 
 server.listen(PORT, () => {
     console.log('server listening on port PORT: ' + PORT);
