e474b5fcf6b2d5713ca69ddbaa581e3f09fff378
Mary <mari.front.74@gmail.com>
2025-05-30 22:07:53 +0500

    fix: исправлена инициализация WebSocket сервера - заменен app.listen() на server.listen() для корректной работы Socket.IO

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,10 +5,10 @@ import messageRoutes from './routes/message.route.js'
 import {connectDB} from "./lib/db.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import { app, server } from "./lib/socket.js";
 
 dotenv.config()
 
-const app = express();
 const PORT = process.env.PORT;
 
 //app.use(express.json())
@@ -28,7 +28,7 @@ app.use('/api/messages', messageRoutes);
 
 
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log('server listening on port PORT: ' + PORT);
     connectDB()
 })
